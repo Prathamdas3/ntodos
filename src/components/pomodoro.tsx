0aa3f8cbdfb31a5fbc91e693cb0c37b68a0c9795
Pratham Das <prathamdas003@gmail.com>
2024-04-26 04:15:49 +0530

    feat(app): pomodoro init

--- src/components/pomodoro.tsx
+++ src/components/pomodoro.tsx
@@ -1,19 +1,51 @@
+'use client'
+import { useState } from 'react'
+
 export default function Promo() {
+  //Todo fix this pomodoro section and then its complete
+  const [mins, setMins] = useState<number>(25)
+  const [secs, setSecs] = useState<number>(0)
+  const [isOn, setIsOn] = useState<boolean>(false)
+
+  setInterval(() => setMins(mins > 0 ? mins - 1 : 0), 60 * 1000)
+  setInterval(() => setSecs(secs > 0 ? secs - 1 : 0), 1000)
+
   return (
     <section className="flex flex-col justify-center items-center min-h-full gap-2">
       <div className="flex gap-2">
-        <input
-          className="font-bold text-2xl bg-transparent inline-block w-8"
-          placeholder="25"
-        />
+        {isOn ? (
+          <p className="font-bold text-2xl bg-transparent inline-block w-8">
+            {mins}
+          </p>
+        ) : (
+          <input
+            className="font-bold text-2xl bg-transparent inline-block w-8"
+            placeholder={`${mins}`}
+            defaultValue={isOn ? 25 : mins}
+            disabled={isOn}
+            onChange={(e) => setMins(Number(e.target.value))}
+          />
+        )}
+
         <span className="bold text-2xl">:</span>
-        <input
-          className="font-bold text-2xl bg-transparent inline-block w-8"
-          placeholder="00"
-        />
+        {isOn ? (
+          <p className="font-bold text-2xl bg-transparent inline-block w-8">
+            {secs}
+          </p>
+        ) : (
+          <input
+            className="font-bold text-2xl bg-transparent inline-block w-8"
+            placeholder={`${secs}`}
+            defaultValue={isOn ? 0 : secs}
+            disabled={isOn}
+            onChange={(e) => setSecs(Number(e.target.value))}
+          />
+        )}
       </div>
 
-      <button className="font-bold text-xl">Start</button>
+      <button className="font-bold text-xl" onClick={() => setIsOn(!isOn)}>
+        {isOn ? 'Pause' : 'Start'}
+      </button>
     </section>
   )
 }
