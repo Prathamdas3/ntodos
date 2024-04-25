d94318bfd23907cf240b85cfd60cfa390e3f270a
Pratham Das <prathamdas003@gmail.com>
2024-04-26 04:15:20 +0530

    feat(app): copleted the extra part

--- src/components/extra.tsx
+++ src/components/extra.tsx
@@ -3,16 +3,9 @@ import { Settings, CircleUser, Gem } from 'lucide-react'
 export default function Extra() {
   return (
     <section className="min-h-full flex flex-col justify-center">
-      <div className="flex justify-center gap-6">
-        <div>
-          <Gem size={70} />
-        </div>
-        <div>
-          <Settings size={70} />
-        </div>
-        <div>
-          <CircleUser size={70} />
-        </div>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h2 className="text-3xl font-bold">Hi There</h2>
+        <p className="">Welcome to my simple listing website </p>
       </div>
     </section>
   )
